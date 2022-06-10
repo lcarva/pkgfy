81d20e2fa6d402b8febe2019302274496bdd7986
Luiz Carvalho <lucarval@redhat.com>
2022-06-10 14:12:11 -0400

    feat: Resolve filename dynamically
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- internal/core/core.go
+++ internal/core/core.go
@@ -1,10 +1,15 @@
 package core
 
 import (
+	"crypto/md5"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 type PkgfyConfig struct {
@@ -20,9 +25,8 @@ type Pkgfy struct {
 	Client *HTTPClient
 }
 
-// Install downloads the given url and saves it in the config.InstallDir.
-// It creates the config.InstallDir if needed. The file mode for the created
-// dir and file is 0755.
+// Install downloads the given url and saves it in the config.InstallDir. It creates the
+// config.InstallDir if needed. The file mode for the created dir and file is 0755.
 func (p *Pkgfy) Install(url string) (err error) {
 	// TODO: Make mode configurable
 	err = os.MkdirAll(p.Config.InstallDir, 0755)
@@ -31,30 +35,33 @@ func (p *Pkgfy) Install(url string) (err error) {
 	}
 	err = nil
 
-	filepath, err := p.download(url, p.Config.InstallDir)
+	filePath, err := p.download(url, p.Config.InstallDir)
 	// TODO: Make mode configurable
-	err = os.Chmod(filepath, 0755)
+	err = os.Chmod(filePath, 0755)
 	if err != nil {
 		return
 	}
 	return
 }
 
-// download fetches the url and saves the response as a new file in the
-// given dir. The filename is hard-coded to "gonzo". The dir is expected
-// to already exist.
-func (p *Pkgfy) download(url, dir string) (filepath string, err error) {
-	// TODO: Extract this from the response header or the URL
-	filename := "gonzo"
-
+// download fetches the url and saves the response as a new file in the given dir. The dir is
+// expected to already exist.
+func (p *Pkgfy) download(url, dir string) (filePath string, err error) {
 	rsp, err := (*p.Client).Get(url)
 	if err != nil {
 		return
 	}
 	defer rsp.Body.Close()
 
-	filepath = path.Join(dir, filename)
-	file, err := os.Create(filepath)
+	filename, err := extractFilename(rsp)
+	if err != nil {
+		return
+	}
+
+	filePath = path.Join(dir, filename)
+
+	tempFilePath := filePath + ".part"
+	file, err := os.Create(tempFilePath)
 	if err != nil {
 		return
 	}
@@ -65,5 +72,39 @@ func (p *Pkgfy) download(url, dir string) (filepath string, err error) {
 		return
 	}
 
+	err = os.Rename(tempFilePath, filePath)
+	return
+}
+
+// extractFilename retrieves a filename from the response by inspecting the Content-Disposition
+// header and falling back to the last path element in the URL.
+func extractFilename(response *http.Response) (filename string, err error) {
+	contentDisposition := response.Header.Get("content-disposition")
+	if contentDisposition != "" {
+		filenameRgx := regexp.MustCompile(`filename=(.+)`)
+		for _, candidates := range filenameRgx.FindAllStringSubmatch(contentDisposition, -1) {
+			candidate := basePath(candidates[len(candidates)-1])
+			candidate = strings.Trim(candidate, `"`)
+			if candidate != "" {
+				filename = candidate
+			}
+		}
+	}
+	if filename == "" {
+		filename = basePath(response.Request.URL.Path)
+	}
+	if filename == "" {
+		// We're really out of options here. Use the hashed value of the url to avoid collisions.
+		filename = fmt.Sprintf("%x", md5.Sum([]byte(response.Request.URL.Redacted())))
+	}
+	return
+}
+
+// basePath returns the last path element of fullPath. If one is not found, "" is returned.
+func basePath(fullPath string) (base string) {
+	candidate := filepath.Base(fullPath)
+	if candidate != "." && candidate != ".." && candidate != string(os.PathSeparator) {
+		base = candidate
+	}
 	return
 }
--- internal/core/core_test.go
+++ internal/core/core_test.go
@@ -5,6 +5,7 @@ import (
 	"io/fs"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
 	"testing"
@@ -20,44 +21,91 @@ func (c *FakeHTTPClient) Get(url string) (*http.Response, error) {
 }
 
 func TestInstall(t *testing.T) {
-	content := []byte("beep-bop-beep")
-	body := ioutil.NopCloser(bytes.NewReader(content))
-	var actualUrl string
-	client := HTTPClient(&FakeHTTPClient{
-		mockGet: func(u string) (*http.Response, error) {
-			actualUrl = u
-			return &http.Response{
-				StatusCode: 200,
-				Body:       body,
-			}, nil
-		},
-	})
-
-	installDir := path.Join(t.TempDir(), "bin")
-	p := Pkgfy{Config: PkgfyConfig{InstallDir: installDir}, Client: &client}
-	url := "https://example.com/gonzo"
-	err := p.Install(url)
-	if err != nil {
-		t.Error(err)
-	}
-	if actualUrl != url {
-		t.Errorf("Expected %s, got %s", url, actualUrl)
+	var tests = []struct {
+		filename string
+		fileurl  string
+		headers  *http.Header
+	}{
+		{"gonzo", "https://example.com/gonzo", &http.Header{}},
+		// Content-Disposition header does not include a filename
+		{"gonzo", "https://example.com/gonzo", cdHeader([]string{"attachement"})},
+		// Content-Disposition header includes a filename
+		{"gonzo", "https://example.com/x", cdHeader([]string{`attachment; filename="gonzo"`})},
+		// Content-Disposition header includes a filename without quotes
+		{"gonzo", "https://example.com/x", cdHeader([]string{`attachment; filename=gonzo`})},
+		// Multiple Content-Disposition headers include different filenames
+		{"gonzo", "https://example.com/x",
+			cdHeader([]string{`attachment; filename="gonzo"`, `attachment; filename="y"`})},
+		// Default to hash of URL
+		{"182ccedb33a9e03fbf1079b209da1a31", "https://example.com/", &http.Header{}},
 	}
 
-	fi, err := os.Lstat(path.Join(installDir, "gonzo"))
-	if err != nil {
-		t.Error(err)
-	}
-	expectedMode := fs.FileMode(0755)
-	if fi.Mode() != expectedMode {
-		t.Errorf("Expected %v, got %v", expectedMode, fi.Mode())
-	}
+	for _, test := range tests {
+		content := "hello, gonzo!"
+		body := ioutil.NopCloser(bytes.NewReader([]byte(content)))
+		var actualUrl string
+		parsedUrl, err := url.Parse(test.fileurl)
+		if err != nil {
+			t.Fatalf("Unable to parse test url, %q: %s", test.fileurl, err)
+		}
+		client := HTTPClient(&FakeHTTPClient{
+			mockGet: func(u string) (*http.Response, error) {
+				actualUrl = u
+				return &http.Response{
+					StatusCode: 200,
+					Header:     *test.headers,
+					Body:       body,
+					Request:    &http.Request{URL: parsedUrl},
+				}, nil
+			},
+		})
 
-	actualContent, err := os.ReadFile(path.Join(installDir, "gonzo"))
-	if err != nil {
-		t.Error(err)
+		installDir := path.Join(t.TempDir(), "bin")
+		p := Pkgfy{Config: PkgfyConfig{InstallDir: installDir}, Client: &client}
+		url := test.fileurl
+		err = p.Install(test.fileurl)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if actualUrl != url {
+			t.Fatalf("Expected %s, got %s", url, actualUrl)
+		}
+
+		files, err := ioutil.ReadDir(installDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		for _, f := range files {
+			if f.Name() != test.filename {
+				t.Fatalf("Found unexpected file in install dir: %q", f.Name())
+			}
+			expectedMode := fs.FileMode(0755)
+			if f.Mode() != expectedMode {
+				t.Fatalf("Expected %v, got %v", expectedMode, f.Mode())
+			}
+		}
+
+		expectedFilename := path.Join(installDir, test.filename)
+
+		actualContent, err := os.ReadFile(expectedFilename)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(actualContent) != string(content) {
+			t.Fatalf("Expected %s, got %s", content, actualContent)
+		}
 	}
-	if string(actualContent) != string(content) {
-		t.Errorf("Expected %s, got %s", content, actualContent)
-	}
+}
+
+// cdHeader returns Headers with given Content-Disposition values.
+func cdHeader(values []string) *http.Header {
+	// values := make([]string, 0, len(filenames))
+	// for _, filename := range filenames {
+	// 	// value := "attachment"
+	// 	// if filename != "" {
+	// 	// 	value = fmt.Sprintf("%s; filename=%q", value, filename)
+	// 	// }
+	// 	values = append(values, value)
+	// }
+	return &http.Header{http.CanonicalHeaderKey("content-disposition"): values}
 }
